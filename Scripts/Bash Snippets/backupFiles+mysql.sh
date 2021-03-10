e3236cb2e1e5394019916342d012c49e693e7c18
Titouan Manachem <titouan.manachem@actimage.com>
2021-03-10 16:24:03 +0100

    THXS AKHIL

--- Scripts/Bash Snippets/backupFiles+mysql.sh
+++ Scripts/Bash Snippets/backupFiles+mysql.sh
@@ -2,6 +2,4 @@
 sudo tar -cvf $(echo backup`date +%d%m%Y`.tar.gz) foldername
 
 #dump sql file with date in name
-sudo touch $(echo sqlDump`date +%d%m%Y`.sql)
-sudo chmod 777 sqlDump19052020.sql
-mysqldump -u username database -p > $(echo sqlDump`date +%d%m%Y`.sql)
+mysqldump -u username database --no-tablespaces -p > $(echo sqlDump`date +%d%m%Y`.sql)
